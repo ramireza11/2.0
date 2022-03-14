82438465827eb661b3b155318b49ad5072c8a038
ramireza11 <angel.g.ramirez@sjsu.edu>
2022-03-13 18:15:13 -0700

    update

--- sketch.js
+++ sketch.js
@@ -1,7 +1,42 @@
+let sins = [{
+  name: "pride",
+  color: "purple"
+}, {
+  name: "wrath",
+  color: "red"
+} {
+  name: "gluttony",
+  color: "orange"
+} {
+  name: "envy",
+  color: "green"
+} {
+  name: "lust",
+  color: "red"
+} {
+  name: "greed",
+  color: "yellow"
+} {
+  name: "sloth",
+  color: "blue"
+}];
+
+let randomIndex;
+
 function setup() {
   createCanvas(400, 400);
+  background(200);
+
 }
 
 function draw() {
-  background(220);
+
+}
+
+function mousePressed() {
+  background(random(200, 255));
+  randomIndex = int(random(dogs.length));
+  text(sins[randomIndex].name, 50, 50);
+  dogs.splice(randomIndex, 1);
+
 }
